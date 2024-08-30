270b91ff319b9ec85364270ba7185a494ff07690
Todd_사봉석 <bssa@maxst.com>
2024-08-30 17:09:01 +0900

    [a-better-environment] Distant Hills * 차량 자동/동적 로드 추가 * 동적로드 바퀴 애니메이션 확인 필요

--- src/components/pages/betterEnvironmentPage/betterEnvironmentPage.tsx
+++ src/components/pages/betterEnvironmentPage/betterEnvironmentPage.tsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import SceneComponent from 'babylonjs-hook';
 import {
+  AbstractMesh,
+  Mesh,
   MeshBuilder,
   Scene,
   SceneLoader,
   StandardMaterial,
   Texture,
   Vector3,
+  Animation,
 } from '@babylonjs/core';
 import { createScene } from '@/utils/createScene';
 import '@babylonjs/loaders';
-import { buildDwellings } from '@/utils/builds';
+import { buildDwellings, buildCar } from '@/utils/builds';
 
 const BetterEnvironmentPage = () => {
-  const onSceneReady = (scene: Scene) => {
+  const runWheelAnimation = (scene: Scene, car: Mesh) => {
+    const meshes = car.getChildMeshes();
+    meshes.map((wheel) => {
+      scene.beginAnimation(wheel, 0, 30, true);
+    });
+  };
+
+  const runCarAnimation = (scene: Scene) => {
+    const animCar = new Animation(
+      'carAnimation',
+      'position.z',
+      30,
+      Animation.ANIMATIONTYPE_FLOAT,
+      Animation.ANIMATIONLOOPMODE_CYCLE,
+    );
+
+    const carKeys: { frame: number; value: number }[] = [];
+    carKeys.push({ frame: 0, value: 8 });
+    carKeys.push({ frame: 150, value: -7 });
+    carKeys.push({ frame: 200, value: -7 });
+    animCar.setKeys(carKeys);
+
+    const car: AbstractMesh = scene.getMeshByName('car') as AbstractMesh;
+    car.animations = [];
+    car.animations.push(animCar);
+
+    scene.beginAnimation(car, 0, 200, true);
+  };
+
+  const onSceneReady = async (scene: Scene) => {
     const { canvas, camera, light } = createScene(scene);
 
     // // 실제 작업할 맵
@@ -54,6 +86,65 @@ const BetterEnvironmentPage = () => {
       null,
       '.glb',
     );
+
+    /* ----- Car ----- */
+    // const car = buildCar(scene);
+    // car.rotation = new Vector3(Math.PI / 2, 0, -Math.PI / 2);
+    // car.rotation = new Vector3(-Math.PI / 2, Math.PI, -Math.PI / 2);
+    // car.position.y = 0.16;
+    // car.position.x = 3;
+    // car.position.z = 8;
+
+    // runWheelAnimation(scene, car);
+    // runCarAnimation(scene);
+
+    /** 동적 로드 */
+    const carLoaderResult = await SceneLoader.ImportMeshAsync(
+      '',
+      'https://assets.babylonjs.com/meshes/',
+      'car.glb',
+    );
+    const car = scene.getMeshByName('car') as Mesh;
+    car.rotation = new Vector3(Math.PI / 2, 0, -Math.PI / 2);
+    car.position.y = 0.16;
+    car.position.x = -3;
+    car.position.z = 8;
+
+    const animCar = new Animation(
+      'carAnimation',
+      'position.z',
+      30,
+      Animation.ANIMATIONTYPE_FLOAT,
+      Animation.ANIMATIONLOOPMODE_CYCLE,
+    );
+
+    const carKeys = [];
+
+    carKeys.push({
+      frame: 0,
+      value: 10,
+    });
+
+    carKeys.push({
+      frame: 200,
+      value: -15,
+    });
+
+    animCar.setKeys(carKeys);
+
+    car.animations = [];
+    car.animations.push(animCar);
+
+    scene.beginAnimation(car, 0, 200, true);
+    const wheelRB = scene.getMeshByName('wheelRB');
+    const wheelRF = scene.getMeshByName('wheelRF');
+    const wheelLB = scene.getMeshByName('wheelLB');
+    const wheelLF = scene.getMeshByName('wheelLF');
+
+    scene.beginAnimation(wheelRB, 0, 30, true);
+    scene.beginAnimation(wheelRF, 0, 30, true);
+    scene.beginAnimation(wheelLB, 0, 30, true);
+    scene.beginAnimation(wheelLF, 0, 30, true);
   };
 
   return (
